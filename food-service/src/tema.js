5f9cb5287ca58cf5ff909e9c0950a5e2d3bbe525
NataliBorshch <67202688+NataliBorshch@users.noreply.github.com>
2020-12-07 20:32:05 +0200

    правки 1

--- food-service/src/tema.js
+++ food-service/src/tema.js
@@ -6,23 +6,23 @@ const Theme = {
 const checkboxRef = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
 
-const changeTema = event => {
+const changeTheme = event => {
   if (checkboxRef.checked) {
-    body.classList.add(Theme.DARK);
-    body.classList.remove(Theme.LIGHT);
-    localStorage.setItem('Thema', Theme.DARK);
+    replaceTheme(Theme.LIGHT, Theme.DARK);
   } else {
-    body.classList.remove(Theme.DARK);
-    body.classList.add(Theme.LIGHT);
-    localStorage.setItem('Thema', Theme.LIGHT);
+    replaceTheme(Theme.DARK, Theme.LIGHT);
   }
 };
 
-function chooseThema() {
-  const currentThema = localStorage.getItem('Thema');
-  if (currentThema === null) {
-    body.classList.add(Theme.LIGHT);
-  } else if (currentThema === Theme.DARK) {
+const replaceTheme = (oldTheme, newTheme) => {
+  body.classList.add(newTheme);
+  body.classList.remove(oldTheme);
+  localStorage.setItem('Theme', newTheme);
+};
+
+function chooseTheme() {
+  const currentThema = localStorage.getItem('Theme');
+  if (currentThema === Theme.DARK) {
     body.classList.add(Theme.DARK);
     checkboxRef.checked = true;
   } else {
@@ -30,5 +30,5 @@ function chooseThema() {
   }
 }
 
-checkboxRef.addEventListener('change', changeTema);
-chooseThema();
+checkboxRef.addEventListener('change', changeTheme);
+chooseTheme();
